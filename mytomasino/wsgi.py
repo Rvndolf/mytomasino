b1d4761bce99e27c509bfc2569457b6c876c9af1
Randolf Amaranto <randolfamaranto@Randolfs-MacBook-Air.local>
2026-02-26 20:51:52 +0800

    fixed settings

--- mytomasino/wsgi.py
+++ mytomasino/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testinglations.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mytomasino.settings")
 
 application = get_wsgi_application()
