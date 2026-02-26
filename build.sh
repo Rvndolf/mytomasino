85a4349a9211dc62d16c8a0a9774b450e282b9bd
Randolf Amaranto <randolfamaranto@Randolfs-MacBook-Air.local>
2026-02-26 21:12:03 +0800

    add superuser

--- build.sh
+++ build.sh
@@ -5,3 +5,9 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@ust-legazpi.edu.ph', 'yourpassword')
+"
+python create_staff.py
