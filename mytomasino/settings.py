743dc22b383b3f18fc5d91997187f2f246f80b2a
Randolf Amaranto <randolfamaranto@Randolfs-MacBook-Air.local>
2026-02-27 23:18:58 +0800

    add cloudinary storage

--- mytomasino/settings.py
+++ mytomasino/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    'cloudinary_storage',
     "django.contrib.staticfiles",
+    'cloudinary',
     "django.contrib.sites",
     "user",
     "dashboard",
@@ -57,6 +59,16 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "mytomasino.middleware.SessionTimeoutMiddleware",
 ]
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
