d9703472e8df005a5f19fda040bbf5182c9e4eef
Randolf Amaranto <randolfamaranto@Randolfs-MacBook-Air.local>
2026-02-28 06:23:26 +0800

    fix migrations

--- build.sh
+++ build.sh
@@ -4,11 +4,10 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth.models import User
 User.objects.filter(username='admin').delete()
 User.objects.create_superuser('admin', 'admin@ust-legazpi.edu.ph', 'Admin_123')
 "
-python create_staff.py
-python create_students.py
