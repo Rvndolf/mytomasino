cb08be906209c37ddaf336c4ba0067c6d73ea293
Randolf Amaranto <randolfamaranto@Randolfs-MacBook-Air.local>
2026-03-01 12:37:09 +0800

    fixed text

--- build.sh
+++ build.sh
@@ -11,4 +11,3 @@ from django.contrib.auth.models import User
 User.objects.filter(username='admin').delete()
 User.objects.create_superuser('admin', 'admin@ust-legazpi.edu.ph', 'Admin_123')
 "
-python reset_students.py
