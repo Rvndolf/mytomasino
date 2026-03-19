ece417971f2396552dacbb25234b128821a60b15
Randolf Amaranto <randolfamaranto@Randolfs-MacBook-Air.local>
2026-03-19 18:53:13 +0800

    fixzki

--- mytomasino/settings.py
+++ mytomasino/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'cloudinary',
+    'cloudinary_storage',
     "django.contrib.sites",
     "user",
     "dashboard",
